92606148beebd1eec13756e04e212db10ff0dd17
Stefan Filges <filges.stefan@gmail.com>
2020-11-19 10:37:20 +0100

    Update server.R

--- umiAnalyzer/inst/shiny/server.R
+++ umiAnalyzer/inst/shiny/server.R
@@ -279,15 +279,15 @@ server <- function(input, output, session, plotFun) {
   # Set up user_data_main
   user_data_main <- reactive({
 
-    if(!is.null(path_to_umierrorcorrect_data)){
-      main <- path_to_umierrorcorrect_data
-    }
-
     # Path selected by the user
     main <- shinyFiles::parseDirPath(
       roots = volumes,
       selection = input$dir
     )
+
+    if(!is.null(path_to_umierrorcorrect_data)){
+      main <- path_to_umierrorcorrect_data
+    }
 
     # Create umiExperiment object
     if (identical(main, character(0))){
